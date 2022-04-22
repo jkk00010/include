ac27402efa3cb4d2e433a2a2f51e9ac34accf084
Jakob Kaivo <jkk@ung.org>
2022-04-21 21:13:07 -0400

    cleanup

--- stddef.h
+++ stddef.h
@@ -27,45 +27,22 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-/* ./src/stddef/NULL.c */
 #define NULL                                                          ((void*)0)
-/* ./src/stddef/offsetof.c */
 #define offsetof(__type, __member)  ((size_t)((void*)&(((__type*)0)->__member)))
 
-/* ./src/stddef/ptrdiff_t.c */
-#ifndef __TYPE_ptrdiff_t_DEFINED__
-#define __TYPE_ptrdiff_t_DEFINED__
 #ifdef __LLP64__
-# if ! defined __STDC_VERSION__ || __STDC_VERSION__ < 199909L
+# if !(defined __STDC_VERSION__ && 199901L < __STDC_VERSION__)
 typedef __int64                                                       ptrdiff_t;
-# else
-typedef long long int                                                 ptrdiff_t;
-# endif
-#else
-typedef long int                                                      ptrdiff_t;
-#endif
-#endif
-
-/* ./src/stddef/size_t.c */
-#ifndef __TYPE_size_t_DEFINED__
-#define __TYPE_size_t_DEFINED__
-#ifdef __LLP64__
-# if !defined __STDC_VERSION__ || __STDC_VERSION__ < 199909L
 typedef unsigned __int64                                                 size_t;
 # else
+typedef long long int                                                 ptrdiff_t;
 typedef unsigned long long int                                           size_t;
 # endif
 #else
+typedef long int                                                      ptrdiff_t;
 typedef unsigned long int                                                size_t;
 #endif
-#endif
 
-/* ./src/stddef/wchar_t.c */
-#ifndef __TYPE_wchar_t_DEFINED__
-#define __TYPE_wchar_t_DEFINED__
 typedef int                                                             wchar_t;
-#endif
-
-
 
 #endif
