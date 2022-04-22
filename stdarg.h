9d2095a22226b1d3d2c4e1331fe7d517f0f9abce
Jakob Kaivo <jkk@ung.org>
2022-04-21 21:10:22 -0400

    cleanup

--- stdarg.h
+++ stdarg.h
@@ -27,24 +27,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-/* ./src/stdarg/va_arg.c */
 #define va_arg(__ap, __type)                      __builtin_va_arg(__ap, __type)
-/* ./src/stdarg/va_end.c */
 #define va_end(__ap)                                      __builtin_va_end(__ap)
-/* ./src/stdarg/va_start.c */
 #define va_start(__ap, __parmN)                __builtin_va_start(__ap, __parmN)
 
-#if	(defined __STDC_VERSION__ && 199901 <= __STDC_VERSION__)
-/* ./src/stdarg/va_copy.c */
+#if (defined __STDC_VERSION__ && 199901 <= __STDC_VERSION__)
 #define va_copy(__dest, __src)                  __builtin_va_copy(__dest, __src)
 #endif
 
-/* ./src/stdarg/va_list.c */
-#ifndef __TYPE_va_list_DEFINED__
-#define __TYPE_va_list_DEFINED__
 typedef __builtin_va_list                                               va_list;
-#endif
-
-
 
 #endif
