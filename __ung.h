d43c9c6a130de11900f481bf6abf77abe10721e3
Jakob Kaivo <jkk@ung.org>
2024-01-29 13:50:07 -0500

    check for keywords defined as macros

---
+++ __ung.h
@@ -0,0 +1,175 @@
+#ifdef auto
+#error Undefined Behavior: Keyword auto is defined as a macro
+#endif
+
+#ifdef break
+#error Undefined Behavior: Keyword break is defined as a macro
+#endif
+
+#ifdef case
+#error Undefined Behavior: Keyword case is defined as a macro
+#endif
+
+#ifdef char
+#error Undefined Behavior: Keyword char is defined as a macro
+#endif
+
+#ifdef const
+#error Undefined Behavior: Keyword const is defined as a macro
+#endif
+
+#ifdef continue
+#error Undefined Behavior: Keyword continue is defined as a macro
+#endif
+
+#ifdef default
+#error Undefined Behavior: Keyword default is defined as a macro
+#endif
+
+#ifdef do
+#error Undefined Behavior: Keyword do is defined as a macro
+#endif
+
+#ifdef double
+#error Undefined Behavior: Keyword double is defined as a macro
+#endif
+
+#ifdef else
+#error Undefined Behavior: Keyword else is defined as a macro
+#endif
+
+#ifdef enum
+#error Undefined Behavior: Keyword enum is defined as a macro
+#endif
+
+#ifdef extern
+#error Undefined Behavior: Keyword extern is defined as a macro
+#endif
+
+#ifdef float
+#error Undefined Behavior: Keyword float is defined as a macro
+#endif
+
+#ifdef for
+#error Undefined Behavior: Keyword for is defined as a macro
+#endif
+
+#ifdef goto
+#error Undefined Behavior: Keyword goto is defined as a macro
+#endif
+
+#ifdef if
+#error Undefined Behavior: Keyword if is defined as a macro
+#endif
+
+#ifdef inline
+#error Undefined Behavior: Keyword inline is defined as a macro
+#endif
+
+#ifdef int
+#error Undefined Behavior: Keyword int is defined as a macro
+#endif
+
+#ifdef long
+#error Undefined Behavior: Keyword long is defined as a macro
+#endif
+
+#ifdef register
+#error Undefined Behavior: Keyword register is defined as a macro
+#endif
+
+#ifdef restrict
+#error Undefined Behavior: Keyword restrict is defined as a macro
+#endif
+
+#ifdef return
+#error Undefined Behavior: Keyword return is defined as a macro
+#endif
+
+#ifdef short
+#error Undefined Behavior: Keyword short is defined as a macro
+#endif
+
+#ifdef signed
+#error Undefined Behavior: Keyword signed is defined as a macro
+#endif
+
+#ifdef sizeof
+#error Undefined Behavior: Keyword sizeof is defined as a macro
+#endif
+
+#ifdef static
+#error Undefined Behavior: Keyword static is defined as a macro
+#endif
+
+#ifdef struct
+#error Undefined Behavior: Keyword struct is defined as a macro
+#endif
+
+#ifdef switch
+#error Undefined Behavior: Keyword switch is defined as a macro
+#endif
+
+#ifdef typedef
+#error Undefined Behavior: Keyword typedef is defined as a macro
+#endif
+
+#ifdef union
+#error Undefined Behavior: Keyword union is defined as a macro
+#endif
+
+#ifdef unsigned
+#error Undefined Behavior: Keyword unsigned is defined as a macro
+#endif
+
+#ifdef void
+#error Undefined Behavior: Keyword void is defined as a macro
+#endif
+
+#ifdef volatile
+#error Undefined Behavior: Keyword volatile is defined as a macro
+#endif
+
+#ifdef while
+#error Undefined Behavior: Keyword while is defined as a macro
+#endif
+
+#ifdef _Alignas
+#error Undefined Behavior: Keyword _Alignas is defined as a macro
+#endif
+
+#ifdef _Alignof
+#error Undefined Behavior: Keyword _Alignof is defined as a macro
+#endif
+
+#ifdef _Atomic
+#error Undefined Behavior: Keyword _Atomic is defined as a macro
+#endif
+
+#ifdef _Bool
+#error Undefined Behavior: Keyword _Bool is defined as a macro
+#endif
+
+#ifdef _Complex
+#error Undefined Behavior: Keyword _Complex is defined as a macro
+#endif
+
+#ifdef _Generic
+#error Undefined Behavior: Keyword _Generic is defined as a macro
+#endif
+
+#ifdef _Imaginary
+#error Undefined Behavior: Keyword _Imaginary is defined as a macro
+#endif
+
+#ifdef _Noreturn
+#error Undefined Behavior: Keyword _Noreturn is defined as a macro
+#endif
+
+#ifdef _Static_assert
+#error Undefined Behavior: Keyword _Static_assert is defined as a macro
+#endif
+
+#ifdef _Thread_local
+#error Undefined Behavior: Keyword _Thread_local is defined as a macro
+#endif
