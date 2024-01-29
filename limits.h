fa44221fc0a17382de99ff2a01e5aa6a13505e69
Jakob Kaivo <jkk@ung.org>
2024-01-29 14:25:13 -0500

    include __ung.h

--- limits.h
+++ limits.h
@@ -31,6 +31,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#include <__ung.h>
+
 /* TODO: XOPEN/POSIX/etc. */
 
 #define CHAR_BIT                                                             (8)
