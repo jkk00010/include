8d51429a5cd51d4c6e52c63349293c69569afae2
Jakob Kaivo <jkk@ung.org>
2024-01-30 15:23:36 -0500

    add ext1 prototypes

--- string.h
+++ string.h
@@ -73,69 +73,85 @@ size_t strxfrm(char * restrict __s1, const char * restrict __s2, size_t __n);
 
 
 #define memchr(__s, __c, __n) \
-	__checked__p(__FILE__, __func__, __LINE__, memchr, __s, __c, __n)
+	__checked_p(__FILE__, __func__, __LINE__, memchr, __s, __c, __n)
 
 #define memcmp(__s1, __s2, __n) \
-	__checked__i(__FILE__, __func__, __LINE__, memcmp, __s1, __s2, __n)
+	__checked_i(__FILE__, __func__, __LINE__, memcmp, __s1, __s2, __n)
 
 #define memcpy(__s1, __s2, __n) \
-	__checked__p(__FILE__, __func__, __LINE__, memcpy, __s1, __s2, __n)
+	__checked_p(__FILE__, __func__, __LINE__, memcpy, __s1, __s2, __n)
 
 #define memmove(__s1, __s2, __n) \
-	__checked__p(__FILE__, __func__, __LINE__, memmove, __s1, __s2, __n)
+	__checked_p(__FILE__, __func__, __LINE__, memmove, __s1, __s2, __n)
 
 #define memset(__s, __c, __n) \
-	__checked__p(__FILE__, __func__, __LINE__, memset, __s, __c, __n)
+	__checked_p(__FILE__, __func__, __LINE__, memset, __s, __c, __n)
 
 #define strcat(__s1, __s2) \
-	(char*)__checked__p(__FILE__, __func__, __LINE__, strcat, __s1, __s2)
+	(char*)__checked_p(__FILE__, __func__, __LINE__, strcat, __s1, __s2)
 
 #define strchr(__s, __c) \
-	(char*)__checked__p(__FILE__, __func__, __LINE__, strchr, __s, __c)
+	(char*)__checked_p(__FILE__, __func__, __LINE__, strchr, __s, __c)
 
 #define strcmp(__s1, __s2) \
-	__checked__i(__FILE__, __func__, __LINE__, strcmp, __s1, __s2)
+	__checked_i(__FILE__, __func__, __LINE__, strcmp, __s1, __s2)
 
 #define strcoll(__s1, __s2) \
-	__checked__i(__FILE__, __func__, __LINE__, strcoll, __s1, __s2)
+	__checked_i(__FILE__, __func__, __LINE__, strcoll, __s1, __s2)
 
 #define strcpy(__s1, __s2) \
-	(char*)__checked__p(__FILE__, __func__, __LINE__, strcpy, __s1, __s2)
+	(char*)__checked_p(__FILE__, __func__, __LINE__, strcpy, __s1, __s2)
 
 #define strcspn(__s1, __s2) \
-	__checked__s(__FILE__, __func__, __LINE__, strcspn, __s1, __s2)
+	__checked_s(__FILE__, __func__, __LINE__, strcspn, __s1, __s2)
 
 #define strerror(__e) \
-	(char*)__checked__p(__FILE__, __func__, __LINE__, strerror, __e)
+	(char*)__checked_p(__FILE__, __func__, __LINE__, strerror, __e)
 
 #define strlen(__s) \
-	__checked__s(__FILE__, __func__, __LINE__, strlen, __s)
+	__checked_s(__FILE__, __func__, __LINE__, strlen, __s)
 
 #define strncat(__s1, __s2, __n) \
-	(char*)__checked__p(__FILE__, __func__, __LINE__, strncat, __s1, __s2, __n)
+	(char*)__checked_p(__FILE__, __func__, __LINE__, strncat, __s1, __s2, __n)
 
 #define strncmp(__s1, __s2, __n) \
-	__checked__i(__FILE__, __func__, __LINE__, strncmp, __s1, __s2, __n)
+	__checked_i(__FILE__, __func__, __LINE__, strncmp, __s1, __s2, __n)
 
 #define strncpy(__s1, __s2, __n) \
-	(char*)__checked__p(__FILE__, __func__, __LINE__, strncpy, __s1, __s2, __n)
+	(char*)__checked_p(__FILE__, __func__, __LINE__, strncpy, __s1, __s2, __n)
 
 #define strpbrk(__s1, __s2) \
-	(char*)__checked__p(__FILE__, __func__, __LINE__, strpbrk, __s1, __s2)
+	(char*)__checked_p(__FILE__, __func__, __LINE__, strpbrk, __s1, __s2)
 
 #define strrchr(__s, __c) \
-	(char*)__checked__p(__FILE__, __func__, __LINE__, strrchr, __s, __c)
+	(char*)__checked_p(__FILE__, __func__, __LINE__, strrchr, __s, __c)
 
 #define strspn(__s1, __s2) \
-	__checked__s(__FILE__, __func__, __LINE__, strspn, __s1, __s2)
+	__checked_s(__FILE__, __func__, __LINE__, strspn, __s1, __s2)
 
 #define strstr(__s1, __s2) \
-	(char*)__checked__p(__FILE__, __func__, __LINE__, strstr, __s1, __s2)
+	(char*)__checked_p(__FILE__, __func__, __LINE__, strstr, __s1, __s2)
 
 #define strtok(__s1, __s2) \
-	(char*)__checked__p(__FILE__, __func__, __LINE__, strtok, __s1, __s2)
+	(char*)__checked_p(__FILE__, __func__, __LINE__, strtok, __s1, __s2)
 
 #define strxfrm(__s1, __s2, __n) \
-	__checked__s(__FILE__, __func__, __LINE__, strxfrm, __s1, __s2, __n)
+	__checked_s(__FILE__, __func__, __LINE__, strxfrm, __s1, __s2, __n)
+
+#ifdef __STDC_WANT_LIB_EXT1__
+typedef int errno_t;
+typedef size_t rsize_t;
+errno_t memcpy_s(void * restrict s1, rsize_t s1max, const void * restrict s2, rsize_t n);
+errno_t memmove_s(void *s1, rsize_t s1max, const void *s2, rsize_t n);
+errno_t strcpy_s(char * restrict s1, rsize_t s1max, const char * restrict s2);
+errno_t strncpy_s(char * restrict s1, rsize_t s1max, const char * restrict s2, rsize_t n);
+errno_t strcat_s(char * restrict s1, rsize_t s1max, const char * restrict s2);
+errno_t strncat_s(char * restrict s1, rsize_t s1max, const char * restrict s2, rsize_t n);
+char *strtok_s(char * restrict s1, rsize_t * restrict s1max, const char * restrict s2, char ** restrict ptr);
+errno_t memset_s(void *s, rsize_t smax, int c, rsize_t n);
+errno_t strerror_s(char *s, rsize_t maxsize, errno_t errnum);
+size_t strerrorlen_s(errno_t errnum);
+size_t strnlen_s(const char *s, size_t maxsize);
+#endif
 
 #endif
