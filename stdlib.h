59f316fcb355df03aabbe88c619d1edcc807d72a
Jakob Kaivo <jkk@ung.org>
2024-06-03 13:19:01 -0400

    add declarations and macros for checked function calls

--- stdlib.h
+++ stdlib.h
@@ -10,7 +10,7 @@ UNG's Not GNU
 
 MIT License
 
-Copyright (c) 2011-2020 Jakob Kaivo <jkk@ung.org>
+Copyright (c) 2011-2024 Jakob Kaivo <jkk@ung.org>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -92,31 +92,19 @@ int atoi(const char * __nptr);
 long int atol(const char * __nptr);
 void * bsearch(const void * __key, const void * __base, size_t __nmemb, size_t __size, int (*__compar)(const void *, const void*));
 void * calloc(size_t __nmemb, size_t __size);
-void * __calloc(const char *, const char *, unsigned long long, size_t, size_t);
-#define calloc(__n, __s) __calloc(__FILE__, __func__, __LINE__, __n, __s)
-
 div_t div(int __numer, int __denom);
 _Noreturn void exit(int __status);
 void free(void * __ptr);
-void __free(const char *, const char *, unsigned long long, void *);
-#define free(__p) __free(__FILE__, __func__, __LINE__, __p)
-
 char * getenv(const char * __name);
 long int labs(long int __j);
 ldiv_t ldiv(long int __numer, long int __denom);
 void * malloc(size_t __size);
-void * __malloc(const char *, const char *, unsigned long long, size_t);
-#define malloc(__n) __malloc(__FILE__, __func__, __LINE__, __n)
-
 int mblen(const char * __s, size_t __n);
 size_t mbstowcs(wchar_t * restrict __pwcs, const char * restrict __s, size_t __n);
 int mbtowc(wchar_t * restrict __pwc, const char * restrict __s, size_t __n);
 void qsort(void * __base, size_t __nmemb, size_t __size, int (*__compar)(const void *, const void *));
 int rand(void);
 void * realloc(void * __ptr, size_t __size);
-void * __realloc(const char *, const char *, unsigned long long, void *, size_t);
-#define realloc(__p, __s) __realloc(__FILE__, __func__, __LINE__, __p, __s)
-
 void srand(unsigned int __seed);
 double strtod(const char * restrict __nptr, char ** restrict __endptr);
 long int strtol(const char * restrict __nptr, char ** restrict __endptr, int __base);
@@ -150,6 +138,116 @@ errno_t qsort_s(void *base, rsize_t nmemb, rsize_t size, int (*compar)(const voi
 errno_t wctomb_s(int * restrict status, char * restrict s, rsize_t smax, wchar_t wc);
 errno_t mbstowcs_s(size_t * restrict retval, wchar_t * restrict dst, rsize_t dstmax, const char * restrict src, rsize_t len);
 errno_t wcstombs_s(size_t * restrict retval, char * restrict dst, rsize_t dstmax, const wchar_t * restrict src, rsize_t len);
+#endif
+
+#ifndef __UNG_INTERNAL__
+_Noreturn void __abort(const char *, const char *, unsigned long long);
+#define abort() __abort(__FILE__, __func__, __LINE__)
+
+int __abs(const char *, const char *, unsigned long long, int __j);
+#define abs(__j) __abs(__FILE__, __func__, __LINE__, __j)
+
+int __atexit(const char *, const char *, unsigned long long, void (*__func)(void));
+#define atexit(__func) (__FILE__, __func__, __LINE__, __func)
+
+double __atof(const char *, const char *, unsigned long long, const char * __nptr);
+#define atof(__nptr) __atof(__FILE__, __func__, __LINE__, __nptr)
+
+int __atoi(const char *, const char *, unsigned long long, const char * __nptr);
+#define atoi(__nptr) __atoi(__FILE__, __func__, __LINE__, __nptr)
+
+long int __atol(const char *, const char *, unsigned long long, const char * __nptr);
+#define atol(__nptr) __atol(__FILE__, __func__, __LINE__, __nptr)
+
+void * __bsearch(const char *, const char *, unsigned long long, const void * __key, const void * __base, size_t __nmemb, size_t __size, int (*__compar)(const void *, const void*));
+#define bsearch(__key, __base, __nmemb, __size, __compar) __bsearch(__FILE__, __func__, __LINE__, __key, __base, __nmemb, __size, __compar)
+
+void * __calloc(const char *, const char *, unsigned long long, size_t, size_t);
+#define calloc(__n, __s) __calloc(__FILE__, __func__, __LINE__, __n, __s)
+
+div_t __div(const char *, const char *, unsigned long long, int __numer, int __denom);
+#define div(__numer, __denom) __div(__FILE__, __func__, __LINE__, __numer, __denom)
+
+_Noreturn void __exit(const char *, const char *, unsigned long long, int __status);
+#define exit(__status) __exit(__FILE__, __func__, __LINE__, __status)
+
+void __free(const char *, const char *, unsigned long long, void *);
+#define free(__p) __free(__FILE__, __func__, __LINE__, __p)
+
+char * __getenv(const char *, const char *, unsigned long long, const char * __name);
+#define getenv(__name) __getenv(__FILE__, __func__, __LINE__, __name)
+
+long int __labs(const char *, const char *, unsigned long long, long int __j);
+#define labs(__j) __labs(__FILE__, __func__, __LINE__, __j)
+
+ldiv_t __ldiv(const char *, const char *, unsigned long long, long int __numer, long int __denom);
+#define ldiv(__numer, __denom) __ldiv(__FILE__, __func__, __LINE__, __numer, __denom)
+
+void * __malloc(const char *, const char *, unsigned long long, size_t);
+#define malloc(__n) __malloc(__FILE__, __func__, __LINE__, __n)
+
+int __mblen(const char *, const char *, unsigned long long, const char * __s, size_t __n);
+#define mblen(__s, __n) __mblen(__FILE__, __func__, __LINE__, __s, __n)
+
+size_t __mbstowcs(const char *, const char *, unsigned long long, wchar_t * restrict __pwcs, const char * restrict __s, size_t __n);
+#define mbstowcs(__pwcs, __s, __n) __mbstowcs(__FILE__, __func__, __LINE__, __pwcs, __s, __n)
+
+int __mbtowc(const char *, const char *, unsigned long long, wchar_t * restrict __pwc, const char * restrict __s, size_t __n);
+#define mbtowc(__pwc, __s, __n) __mbtowc(__FILE__, __func__, __LINE__, __pwc, __s, __n)
+
+void __qsort(const char *, const char *, unsigned long long, void * __base, size_t __nmemb, size_t __size, int (*__compar)(const void *, const void *));
+#define qsort(__base, __nmemb, __size, __compar) __qsort(__FILE__, __func__, __LINE__, __base, __nmemb, __size, __compar)
+
+int __rand(const char *, const char *, unsigned long long);
+#define rand() __rand(__FILE__, __func__, __LINE__)
+
+void * __realloc(const char *, const char *, unsigned long long, void *, size_t);
+#define realloc(__p, __s) __realloc(__FILE__, __func__, __LINE__, __p, __s)
+
+void __srand(const char *, const char *, unsigned long long, unsigned int __seed);
+#define srand(__seed) __srand(__FILE__, __func__, __LINE__, __seed)
+
+double __strtod(const char *, const char *, unsigned long long, const char * restrict __nptr, char ** restrict __endptr);
+#define strtod(__nptr, __endptr) __strtod(__FILE__, __func__, __LINE__, __nptr, __endptr)
+
+long int __strtol(const char *, const char *, unsigned long long, const char * restrict __nptr, char ** restrict __endptr, int __base);
+#define strtol(__nptr, __endptr, __base) __strtol(__FILE__, __func__, __LINE__, __nptr, __endptr, __base)
+
+unsigned long int __strtoul(const char *, const char *, unsigned long long, const char * __nptr, char ** __endptr, int __base);
+#define strtoul(__nptr, __endptr, __base) __strtoul(__FILE__, __func__, __LINE__, __nptr, __endptr, __base)
+
+int __system(const char *, const char *, unsigned long long, const char * __string);
+#define sysem(__string) __system(__FILE__, __func__, __LINE__, __string)
+
+size_t __wcstombs(const char *, const char *, unsigned long long, char * restrict __s, const wchar_t * restrict __pwcs, size_t __n);
+#define wcstombs(__s, __pwcs, __n) __wcstombs(__FILE__, __func__, __LINE__, __s, __pwcs, __n)
+
+int __wctomb(const char *, const char *, unsigned long long, char * __s, wchar_t __wchar);
+#define wctomb(__s, __whcar) __wctomb(__FILE__, __func__, __LINE__, __s, __wchar)
+
+_Noreturn void ___Exit(const char *, const char *, unsigned long long, int __status);
+#define _Exit(__status) ___Exit(__FILE__, __func__, __LINE__, __status)
+
+long long int __atoll(const char *, const char *, unsigned long long, const char *__nptr);
+#define atoll(__nptr) __atoll(__FILE__, __func__, __LINE__, __nptr)
+
+long long int __llabs(const char *, const char *, unsigned long long, long long int __j);
+#define llabs(__j) __llabs(__FILE__, __func__, __LINE__, __j)
+
+lldiv_t __lldiv(const char *, const char *, unsigned long long, long long int __numer, long long int __denom);
+#define lldiv(__numer, __denom) __lldiv(__FILE__, __func__, __LINE__, __numer, __denom)
+
+float __strtof(const char *, const char *, unsigned long long, const char * restrict __nptr, char ** restrict __endptr);
+#define strtof(__nptr, __endptr) __strtof(__FILE__, __func__, __LINE__, __nptr, __endptr)
+
+long double __strtold(const char *, const char *, unsigned long long, const char * restrict __nptr, char ** restrict __endptr);
+#define strtold(__nptr, __endptr) __strtold(__FILE__, __func__, __LINE__, __nptr, __endptr)
+
+long long int __strtoll(const char *, const char *, unsigned long long, const char * restrict __nptr, char ** restrict __endptr, int __base);
+#define strtoll(__nptr, __endptr, __base) __strtoll(__FILE__, __func__, __LINE__, __nptr, __endptr, __base)
+
+unsigned long long int __strtoull(const char *, const char *, unsigned long long, const char * restrict __nptr, char ** restrict __endptr, int __base);
+#define strtoull(__nptr, __endptr, __base) __strtoul(__FILE__, __func__, __LINE__, __nptr, __endptr, __base)
 #endif
 
 #endif
